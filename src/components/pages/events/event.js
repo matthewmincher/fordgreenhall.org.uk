57fc295ea301487adccfe9b8662a8ff698fd3d27
matthewmincher <matt@codefanatics.co.uk>
2021-08-10 13:25:46 +0100

    Improvements for events

--- src/components/pages/events/event.js
+++ src/components/pages/events/event.js
@@ -1,15 +1,38 @@
 import * as React from "react";
 import * as Styles from "./event.module.scss"
 
-const Event = ({title, date, image, children }) => {
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons'
+
+import { ics } from "calendar-link";
+import format from 'date-fns/format';
+
+const Event = ({title, description, startDate, endDate, date, image, children }) => {
+	const event = {
+		title: title,
+		description: description,
+		start: startDate,
+		end: endDate
+	}
+
+	const dateString = startDate && endDate ? format(startDate, 'EEEE do MMMM h:mmaaa') + ' - ' + format(endDate, 'h:mmaaa') : date;
 
 	return (
 		<div className={Styles.container}>
+			{image}
 			<h2>{title}</h2>
 
 			{children}
 
-			<div className={Styles.date}>{date}</div>
+			<div className={Styles.date}>
+				{startDate && endDate ?
+					(<a download={`${title}.ics`} href={ics(event)}>{dateString} <FontAwesomeIcon icon={faCalendarAlt} /></a>)
+					:
+					(<span>{dateString}</span>)
+				}
+			</div>
 		</div>
 	)
 }
