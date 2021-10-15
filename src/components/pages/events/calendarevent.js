3b899513fe5626b22f75171048851e3998278093
Matthew Mincher <matthew@mincher.org>
2021-10-15 01:25:36 +0100

    Progress with new event component (not ready for live yet - needs more mobile testing)

---
+++ src/components/pages/events/calendarevent.js
@@ -0,0 +1,84 @@
+import * as React from "react";
+import * as Styles from "./calendarevent.module.scss"
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import {  } from '@fortawesome/free-regular-svg-icons'
+
+import {useEffect, useState} from "react";
+import {StaticImage} from "gatsby-plugin-image";
+import format from "date-fns/format";
+
+export default function CalendarEvent({ title, image, description, facebookUrl, startDate, endDate, datePrefix, dateDayPart, dateMonthPart, dateTimePart }) {
+
+	const hideFrom = endDate ? new Date(endDate.getTime() + 43200000) : null;
+	const [isVisible, setIsVisible] = useState(!hideFrom || hideFrom >= Date.now());
+
+	useEffect(() => {
+		setIsVisible(!hideFrom || hideFrom >= Date.now());
+	}, []);
+
+	if(!dateDayPart && (startDate || endDate)){
+		dateDayPart = format(startDate ?? endDate, 'do');
+	}
+	if(!dateMonthPart && (startDate || endDate)){
+		dateMonthPart = format(startDate ?? endDate, 'MMMM');
+	}
+	if(!dateTimePart && (startDate || endDate)){
+		dateTimePart = format(startDate ?? endDate, 'h:mmaaa')
+
+		if(startDate && endDate){
+			dateTimePart += ' - ' + format(endDate, 'h:mmaaa');
+		}
+	}
+
+	return (
+		isVisible ?
+		<div className={Styles.container}>
+			<div className={Styles.left}>
+				{datePrefix &&
+					<div className={Styles.prefix}>{datePrefix}</div>
+				}
+
+				{dateDayPart &&
+					<div className={Styles.day}>{dateDayPart}</div>
+				}
+
+				{dateMonthPart &&
+					<div className={Styles.month}>{dateMonthPart}</div>
+				}
+
+				{dateTimePart &&
+					<div className={Styles.time}>{dateTimePart}</div>
+				}
+			</div>
+			<div className={Styles.right}>
+				<div className={Styles.title}>{title}</div>
+				{image &&
+					<div className={Styles.image}>
+						{image}
+					</div>
+				}
+
+				<div className={Styles.description}>
+					{description}
+				</div>
+
+				<div className={Styles.facebookSpacer} />
+
+				{facebookUrl &&
+					<a target="_blank" rel="noreferrer" href={facebookUrl} className={Styles.facebookLink} title="See this event on Facebook">
+						<StaticImage
+							src="../../../images/social/f_logo_RGB-Black_100.png"
+							placeholder="none"
+							layout="fixed"
+							loading="eager"
+							width={25}
+						/>
+					</a>
+				}
+			</div>
+		</div> : null
+	)
+}
--- src/pages/events.js
+++ src/pages/events.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout/layout";
 import * as Styles from "./events.module.scss"
 import CovidMessage from "../components/global/covidmessage";
 import Event from "../components/pages/events/event";
+import CalendarEvent from "../components/pages/events/calendarevent";
 import {StaticImage} from "gatsby-plugin-image";
 import ScrollToTop from "../components/layout/scrolltotop";
 
@@ -19,78 +20,116 @@ const EventsPage = () => {
 				<div className={Styles.container}>
 					<h1>Events</h1>
 
-					<Event
-						title={"Thursday Afternoons\nKnit and Natter Group"}
-						date="Every Thursday afternoon 1:00 - 3:30pm"
-						image={<StaticImage
-							className={Styles.image}
-							src="./../images/copy/events/knitandnatter.png"
-							alt="Basket with balls of yarn and knitting needles"
-							placeholder="none"
-							layout="fixed"
-							loading="eager"/>}>
-						<p>Bring along your knitting to our group. Enjoy a cup of tea and a natter.</p>
-						<p className={Styles.emph}>£2 per person, including refreshments</p>
-					</Event>
+					<CalendarEvent
+						title={"Afternoon Knit and Natter Group"}
+						datePrefix="Every"
+						dateDayPart="Thursday"
+						dateTimePart="1:00pm - 3:30pm"
+						description={
+							<div>
+								<div style={{
+									position: "absolute",
+									bottom: "-60px",
+									right: "-60px",
+									zIndex: 0
+								}}>
+									<StaticImage
+										src="./../images/copy/events/knitandnatter.png"
+										alt="Basket with balls of yarn and knitting needles"
+										placeholder="none"
+										layout="fixed"
 
-					<Event
+										width={200}
+										loading="eager"/>
+								</div>
+								<div style={{
+									position: "relative"
+								}}>
+									<p>Bring along your knitting to our group. Enjoy a cup of tea and a natter.</p>
+									<p className={Styles.emph}>£2 per person, including refreshments</p>
+								</div>
+							</div>
+						}
+					/>
+
+					<CalendarEvent
 						title="The Wisewoman in the 17th Century"
-						description="A talk looking at the role of the Wisewoman in the Stuart age."
-						date="Sunday 17th October - 2pm"
-						endDate={new Date(1634479200000)}
-						image={<StaticImage
-							className={Styles.image}
-							src="../images/copy/events/wise_women.jpg"
-							alt="Framed portrait of an older Stuart woman wearing a black hat with lace trim"
-							placeholder="none"
-							layout="fixed"
-							loading="eager"
-							width={240}
-						/>}>
-						<p>A talk looking at the role of the Wisewoman in the Stuart age.</p>
-						<p className={Styles.emph}>Tickets only. £9 per person.</p>
-						<p>Available from the Hall fordgreenhall.museum@gmail.com<br />01782 537696.</p>
-					</Event>
+						facebookUrl="https://fb.me/e/24NXepnMG"
+						endDate={new Date(1634475600000)}
+						image={
+							<StaticImage
+								src="../images/copy/events/wise_women.jpg"
+								alt="Framed portrait of an older Stuart woman wearing a black hat with lace trim"
+								placeholder="none"
+								layout="fixed"
+								loading="eager"
+								width={200}
+							/>
+						}
+						description={
+							<div>
+								<p>A talk looking at the role of the Wisewoman in the Stuart age.</p>
+								<p className={Styles.emph}>Tickets only. £9 per person.</p>
+								<p>Available from the Hall fordgreenhall.museum@gmail.com<br />01782 537696.</p>
+							</div>
+						}
+					/>
 
-					<Event
+					<CalendarEvent
 						title="Halloween Witch Hunt"
-						description="See one of the most haunted houses in Stoke on Trent decorated for Halloween!"
-						date="25th-28th & 31st October - 13:00 - 15:30"
+						facebookUrl="https://fb.me/e/2B4wu9vEO"
 						endDate={new Date(1635694200000)}
-						image={<StaticImage
-							className={Styles.image}
-							src="../images/copy/events/halloween.jpg"
-							alt="Wooden door with metal studs and trimmings. A Haunted House sign with a skull on it hangs from the knocker."
-							placeholder="none"
-							layout="fixed"
-							loading="eager"
-							width={240}
-						/>}>
-						<p>Search out 17th century Witches around the Hall in our trail.</p>
-						<p className={Styles.emph}>See one of the most haunted houses in Stoke on Trent decorated for Halloween!</p>
-						<p>Normal admission applies (Adults £4.50, Children and Concessions £3.50, Family ticket £13.00, under 5's free.)</p>
-					</Event>
+						dateDayPart={
+							<span style={{
+								fontSize: "80%"
+							}}>
+								25th-28th, 31st
+							</span>
+						}
+						dateMonthPart="October"
+						dateTimePart="1:00pm - 3:30pm"
+						image={
+							<StaticImage
+								src="../images/copy/events/halloween.jpg"
+								alt="Wooden door with metal studs and trimmings. A Haunted House sign with a skull on it hangs from the knocker."
+								placeholder="none"
+								layout="fixed"
+								loading="eager"
+								width={200}
+							/>
+						}
+						description={
+							<div>
+								<p>Search out 17th century Witches around the Hall in our trail.</p>
+								<p className={Styles.emph}>See one of the most haunted houses in Stoke on Trent decorated for Halloween!</p>
+								<p>Normal admission applies (Adults £4.50, Children and Concessions £3.50, Family ticket £13.00, under 5's free.)</p>
+							</div>
+						}
+					/>
 
-					<Event
+					<CalendarEvent
 						title="Christmas Craft Fair"
-						description="Browse craft stalls for that special Christmas gift inside our 17th century house!"
-						date="Sunday 21st November - 11:00 - 15:00"
+						facebookUrl="https://fb.me/e/2PNwFbmgv"
 						startDate={new Date(1637492400000)}
 						endDate={new Date(1637506800000)}
-						image={<StaticImage
-							className={Styles.image}
-							src="../images/copy/events/christmas.jpg"
-							alt="An exterior shot of the Hall seen through the branches of a bare tree. Snow covers the roof and grassy garden."
-							placeholder="none"
-							layout="fixed"
-							loading="eager"
-							width={240}
-						/>}>
-						<p>Browse craft stalls for that special Christmas gift inside our 17th century house!</p>
-						<p className={Styles.emph}>FREE ADMISSION.</p>
-						<p>***All stalls fully booked***</p>
-					</Event>
-
+						image={
+							<StaticImage
+								src="../images/copy/events/christmas.jpg"
+								alt="An exterior shot of the Hall seen through the branches of a bare tree. Snow covers the roof and grassy garden."
+								placeholder="none"
+								layout="fixed"
+								loading="eager"
+								width={200}
+							/>
+						}
+						description={
+							<div>
+								<p>Browse craft stalls for that special Christmas gift inside our 17th century house!</p>
+								<p className={Styles.emph}>FREE ADMISSION.</p>
+								<p>***All stalls fully booked***</p>
+							</div>
+						}
+					/>
 
 					<ScrollToTop />
 
