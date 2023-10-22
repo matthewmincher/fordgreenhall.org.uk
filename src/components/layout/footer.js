95ec55eaa51a9b1a53f80dd2a8f4ae8dc1e6748f
Matthew Mincher <matthew@mincher.org>
2023-10-22 17:14:44 +0100

    Add welcome award to footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -127,12 +127,25 @@ const Footer = () => {
 									alt="" />
 							</a>
 						</div>
-						<div className={Styles.hiddenGem}>
+						<div className={Styles.hiddenGemAward}>
 							<a href="https://www.visitenglandassessmentservices.com/establishments/ford-green-hall/"
 							   target="_blank"
 							   rel="noreferrer">
 								<StaticImage
 									src="../../images/thirdparties/hidden_gem_22.png"
+									height={141}
+									placeholder="none"
+									loading="eager"
+									layout="fixed"
+									alt="" />
+							</a>
+						</div>
+						<div className={Styles.welcomeAward}>
+							<a href="https://www.visitenglandassessmentservices.com/establishments/ford-green-hall/"
+							   target="_blank"
+							   rel="noreferrer">
+								<StaticImage
+									src="../../images/thirdparties/welcome_23.png"
 									height={141}
 									placeholder="none"
 									loading="eager"
